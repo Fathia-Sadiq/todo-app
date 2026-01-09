1c0cb5aa32a146606068c35f7965dd3dda9da70f
Sadiq Fathia <fathiasadiq2019@gmail.com>
2026-01-09 00:08:10 -0600

    defined addTodo() function that allows a new task to be added to the list todos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('todo-app');
+  newTodoTitle = signal('');
+  todos: Todo[] = [];
+
+  addTodo(): void {
+    if (this.newTodoTitle().trim()) {
+      const newTodo: Todo = {
+        id: Date.now(),
+        title: this.newTodoTitle().trim(),
+        completed: false
+      };
+      this.todos.push(newTodo);
+      this.newTodoTitle.set('');
+    }
+
+  }
 }
